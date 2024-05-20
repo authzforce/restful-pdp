887a9e2ad3a84886f933e3fd27c8f4428d16336a
Cyril Dangerville <1372580+cdanger@users.noreply.github.com>
2024-05-21 01:55:46 +0200

    - Changed spring-beans.xml config to allow customization of the service base URL path -> Changed the service URL path to /pdp in tests (instead of /services/pdp), and to / in docker folder, using cxf.path and cxf.jaxrs.server.path properties in application.yaml - Renamed docker-compose.yml to compose.yml in docker folder - Removed testng suite xml file, using JUnit 5 only instead. - Enhanced the PDP's JAX-RS resource implementation (JaxrsPdpResource and PdpBundle classes) to support another JSON-based authorization API (e.g. AuthZEN) when application/json mime type is used, in addition to XACML/XML and XACML/JSON (application/xacml+json) - Upgraded spring-boot-dependencies to 3.1.11, and cxf to 4.0.4 - Added developer and license info to Maven POM - Fixed unit tests in cxf-spring-boot-server module

--- jaxrs/src/main/java/org/ow2/authzforce/rest/pdp/jaxrs/PdpBundle.java
+++ jaxrs/src/main/java/org/ow2/authzforce/rest/pdp/jaxrs/PdpBundle.java
@@ -30,6 +30,7 @@ import org.ow2.authzforce.core.pdp.impl.BasePdpEngine;
 import org.ow2.authzforce.core.pdp.impl.PdpEngineConfiguration;
 import org.ow2.authzforce.core.pdp.impl.io.PdpEngineAdapters;
 import org.ow2.authzforce.core.pdp.impl.io.SingleDecisionXacmlJaxbRequestPreprocessor;
+import org.ow2.authzforce.core.pdp.io.xacml.json.BaseXacmlJsonRequestPreprocessor;
 import org.ow2.authzforce.core.pdp.io.xacml.json.BaseXacmlJsonResultPostprocessor;
 import org.ow2.authzforce.core.pdp.io.xacml.json.SingleDecisionXacmlJsonRequestPreprocessor;
 
@@ -46,20 +47,19 @@ public final class PdpBundle
 	private final CloseablePdpEngine engine;
 	private final PdpEngineInoutAdapter<Request, Response> xacmlJaxbIoAdapter;
 	private final PdpEngineInoutAdapter<JSONObject, JSONObject> xacmlJsonIoAdapter;
+	private final PdpEngineInoutAdapter<JSONObject, JSONObject> defaultJsonIoAdapter;
 
 	/**
 	 * Constructs a PDP engine from {@code pdpConf} and I/O adapters plugged to it
 	 * 
 	 * @param pdpConf
 	 *            PDP engine configuration
-	 * @param enableXacmlJsonProfile
-	 *            true iff XACML/JSON I/O is to be supported
 	 * @throws java.lang.IllegalArgumentException
 	 *             if {@code pdpConf.getXacmlExpressionFactory() == null || pdpConf.getRootPolicyProvider() == null}
 	 * @throws java.io.IOException
 	 *             error closing {@code pdpConf.getRootPolicyProvider()} when static resolution is to be used
 	 */
-	public PdpBundle(final PdpEngineConfiguration pdpConf, final boolean enableXacmlJsonProfile) throws IllegalArgumentException, IOException
+	public PdpBundle(final PdpEngineConfiguration pdpConf) throws IllegalArgumentException, IOException
 	{
 		this.engine = new BasePdpEngine(pdpConf);
 		// did not throw exception, so valid
@@ -76,8 +76,15 @@ public final class PdpBundle
 		this.xacmlJaxbIoAdapter = PdpEngineAdapters.newInoutAdapter(Request.class, Response.class, engine, ioProcChains, extraPdpFeatures -> SingleDecisionXacmlJaxbRequestPreprocessor.LaxVariantFactory.INSTANCE.getInstance(attValFactoryRegistry, isStrictAttIssuerMatchEnabled, isXpathEnabled,
 				extraPdpFeatures), () -> new BaseXacmlJaxbResultPostprocessor(clientReqErrVerbosityLevel));
 
-		this.xacmlJsonIoAdapter = enableXacmlJsonProfile ? PdpEngineAdapters.newInoutAdapter(JSONObject.class, JSONObject.class, engine, ioProcChains, extraPdpFeatures -> SingleDecisionXacmlJsonRequestPreprocessor.LaxVariantFactory.INSTANCE.getInstance(attValFactoryRegistry, isStrictAttIssuerMatchEnabled, isXpathEnabled,
-				extraPdpFeatures), () -> new BaseXacmlJsonResultPostprocessor(clientReqErrVerbosityLevel)) : null;
+		final Entry<DecisionRequestPreprocessor<?, ?>, DecisionResultPostprocessor<?, ?>> jsonIoProcChain = ioProcChains.get(JSONObject.class);
+
+		// Default In-out adapter for JSON requests if any defined by configuration (may be XACML/JSON Profile's one or OpenID AuthZEN for instance)
+		this.defaultJsonIoAdapter = jsonIoProcChain == null ? null: PdpEngineAdapters.newInoutAdapter(JSONObject.class, JSONObject.class, engine, ioProcChains, extraPdpFeatures -> SingleDecisionXacmlJsonRequestPreprocessor.LaxVariantFactory.INSTANCE.getInstance(attValFactoryRegistry, isStrictAttIssuerMatchEnabled, isXpathEnabled,
+				extraPdpFeatures), () -> new BaseXacmlJsonResultPostprocessor(clientReqErrVerbosityLevel));
+
+		// In-out adapter for XACML/JSON Profile's requests if defined by configuration
+		this.xacmlJsonIoAdapter = defaultJsonIoAdapter != null && jsonIoProcChain.getKey() instanceof BaseXacmlJsonRequestPreprocessor ? defaultJsonIoAdapter: null;
+
 	}
 
 	/**
@@ -102,11 +109,21 @@ public final class PdpBundle
 
 	/**
 	 * Get the PDP engine adapter for XACML 3.0/JSON (JSON Profile of XACML 3.0) input/output
-	 * 
-	 * @return the XACML/JSON I/O adapter, null if not supported
+	 *
+	 * @return the XACML/JSON I/O adapter or null if not supported
 	 */
 	public PdpEngineInoutAdapter<JSONObject, JSONObject> getXacmlJsonIoAdapter()
 	{
 		return xacmlJsonIoAdapter;
+	}
+
+	/**
+	 * Get the default PDP engine adapter for JSON input/output
+	 * 
+	 * @return the I/O adapter or null if JSON input not supported at all
+	 */
+	public PdpEngineInoutAdapter<JSONObject, JSONObject> getDefaultJsonIoAdapter()
+	{
+		return defaultJsonIoAdapter;
 	}
 }
